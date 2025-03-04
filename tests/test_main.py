b50d9d7dc3754beba0af7089fd15a8610dedc9d1
Jaafar Alaswad <jalaswad@bu.edu>
2025-03-03 22:51:44 -0500

    Add tests

---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pytest
+
+# Import functions
+from direct_stiffness_method.direct_stiffness_method import Structure, BoundaryConditions, Solver, PostProcessing, BucklingAnalysis, PlotResults
+
+
